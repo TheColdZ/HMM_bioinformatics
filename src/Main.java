49efbaec550ddd5ce2c428c2c7ef85097281b3c7
TheColdZ <44496095+TheColdZ@users.noreply.github.com>
2020-03-01 18:20:22 +0100

    decoding now posible, refactored variables

--- src/Main.java
+++ src/Main.java
@@ -8,58 +8,59 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        viterbi(7,transitions(),emission(),start());
-        //reader();
+        for (int i = 1; i < 6; i++) {
+            viterbi(transitions(),emission(),start(),i);
+        }
     }
 
     /**
      *
-     * @param states number of states
      * @param transition   a quadratic transistion matrix
      * @param emission  an emission probability matrix, |states| x |emission alphabet|
      * @param start vector of starting probabilities.
+     * @param iteration to name the files according to iteration
      * @return
      */
 
-    public static double viterbi(int states, double[][] transition, double[][] emission, double[] start ){
-        //String gene = "ACGATGCGCTAATATGTCCGATGACGTGAGCATAAGCGACAT";
-        String gene = reader();
-        int inputs = gene.length(); //length of input
-        double [][] delta= new double[states][inputs];
-        int [][] psi = new int[states][inputs];
+    public static double viterbi( double[][] transition, double[][] emission, double[] start, int iteration ){
+        int states = transition.length;
+        String gene = reader(iteration);
+        int inputLength = gene.length(); //length of input
+        double [][] delta = new double[states][inputLength];
+        int [][] psi = new int[states][inputLength];
         for (int i = 0; i <states ; i++) {
             delta[i][0] = log(start[i]) + log(emission[i][1]);
             //System.out.println(delta[i][0]);
 
         }
-        for (int j = 0; j < inputs-1 ; j++) {
+        for (int j = 0; j < inputLength-1 ; j++) {
             for (int i = 0; i <states ; i++) {
                 psi[i][j+1]=-1;
                 //System.out.println("input:"+j);
                 //System.out.println("state:"+i);
 
-                double maxTrans = Double.NEGATIVE_INFINITY;
+                double maxTransProb = Double.NEGATIVE_INFINITY;
                 for (int k = 0; k <states ; k++) {
-                    double trans = log(transition[k][i])+delta[k][j];
-                    if(maxTrans < trans){
+                    double transitionProb = log(transition[k][i])+delta[k][j];
+                    if(maxTransProb < transitionProb){
                         psi[i][j+1] = k;
-                        maxTrans = trans;
+                        maxTransProb = transitionProb;
                     }
                 }
-                delta[i][j+1]= maxTrans+log(emissionProbability(i,gene.charAt(j)));
+                delta[i][j+1]= maxTransProb+log(emissionProbability(i,gene.charAt(j)));
                 //System.out.println(delta[i][j]);
                 if(j % 10000 == 0 && i ==0){
                     System.out.println("I'm alive, don't kill me yet!: "+ j);
                 }
             }
         }
-        int [] qstar = new int[inputs];
-        double possible = delta[0][inputs-1];
+        int [] qstar = new int[inputLength];
+        double possibleMaxLikelihood = delta[0][inputLength-1];
         int possibleIndex =0;
         for (int i = 0; i <states ; i++) {
 
-            if(possible<delta[i][inputs-1]){
-                possible = delta[i][inputs-1];
+            if(possibleMaxLikelihood<delta[i][inputLength-1]){
+                possibleMaxLikelihood = delta[i][inputLength-1];
                 possibleIndex = i;
             }
         }
@@ -67,18 +68,18 @@ public class Main {
         //Printing below
         System.out.println("Done John, starting print");
         try{
-            FileWriter fw=new FileWriter("output.txt");
+            FileWriter outputFileWriter = new FileWriter("output"+iteration+".txt");
 
         //String builder2 = "";
         for (int j = 0; j <states ; j++) {  // looper omvendt med j og i fordi pretty print TODO
             StringBuilder sb1 = new StringBuilder();
 
-            for (int i = 0; i <inputs  ; i++) {
+            for (int i = 0; i <inputLength  ; i++) {
                 sb1.append(" "+ (int) delta[j][i]);
                 //builder2 += "  "+psi[j][i];
             }
-             fw.write("J"+j+" "+sb1.toString());
-            fw.write("\n");
+             outputFileWriter.write("J"+j+" "+sb1.toString());
+            outputFileWriter.write("\n");
             //System.out.println(j+sb1.toString());
             //sb1.append("\n");
             //builder2 += "\n";
@@ -87,25 +88,29 @@ public class Main {
         }
         catch (Exception e){}
         //System.out.println(builder2);
-        qstar[inputs-1] = possibleIndex;
-        for (int j = inputs-2; j >0 ; j--) {
+        qstar[inputLength-1] = possibleIndex;
+        for (int j = inputLength-2; j >0 ; j--) {       //Backtracking through the most likely path.
             qstar[j] = psi[qstar[j+1]][j];
         }
         StringBuilder stateStringBuilder = new StringBuilder();    //Stringuilder
-        Map<Integer,Integer> counter = new HashMap<>();
+        StringBuilder decodeStringBuilder = new StringBuilder();
+        Map<Integer,Integer> stateCounter = new HashMap<>();
         for (int i = 0; i < 7; i++) {
-            counter.put(i,0);
+            stateCounter.put(i,0);
         }
-        for (int j = 0; j < inputs; j++) {
+        for (int j = 0; j < inputLength; j++) {
             //System.out.print(qstar[j]+" ");
-            counter.put(qstar[j],counter.get(qstar[j])+1);
+            stateCounter.put(qstar[j],stateCounter.get(qstar[j])+1);
             stateStringBuilder.append(qstar[j]);
+            decodeStringBuilder.append(decodeState7states(qstar[j]));
         }
         System.out.println();
-        System.out.println(counter);
+        System.out.println(stateCounter);
         try {
-            FileWriter fw = new FileWriter("states1.txt");      //Writing states to a file
+            FileWriter fw = new FileWriter("states"+iteration+".txt");      //Writing states to a file
             fw.write(stateStringBuilder.toString());
+            FileWriter fw2 = new FileWriter("decoding"+iteration+".txt");   //Writing decoded states to file
+            fw2.write(decodeStringBuilder.toString());
         }
         catch(Exception e){}
 
@@ -113,6 +118,38 @@ public class Main {
         return 0.1;
     }
 
+    /**
+     * This method decodes the state to a coding, non-coding and reverse coding denoted as C, N R
+     * @param i the state to translate to "coding"
+     * @return  what the given state did.
+     */
+    private static String decodeState7states(int i) {
+        String decode = "";
+        switch(i){
+            case 0: decode = "C";
+                break;
+            case 1: decode = "C";
+                break;
+            case 2: decode = "C";
+                break;
+            case 3: decode = "N";
+                break;
+            case 4: decode = "R";
+                break;
+            case 5: decode = "R";
+                break;
+            case 6: decode = "R";
+                break;
+            default: ;
+        }
+        return decode;
+    }
+
+    /**
+     * Our very own log function, in case of 0 input the method returns negative infinity
+     * @param input double to perform log on
+     * @return  ln(input)
+     */
     private static double log(double input){
         if(input ==0.0){
             return Double.NEGATIVE_INFINITY;
@@ -177,11 +214,12 @@ public class Main {
 
     /**
      * https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
-     * @return
+     * @param iteration which file to read.
+     * @return string containing gene
      */
 
-    public  static String reader() {
-        File file = new File("C:\\Users\\thoma\\Bachelor\\genome2.txt");
+    public  static String reader(int iteration) {
+        File file = new File("C:\\Users\\thoma\\Bachelor\\genome"+iteration+".txt");
         StringBuilder sb1 = new StringBuilder();
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
